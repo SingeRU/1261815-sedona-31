895b9878cb97e44076c42de094862b9235f38bc1
ILIDAN <mrster03@gmail.com>
2021-07-11 13:55:07 +0300

    Внёс правки

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,20 @@
 let openButton = document.querySelector('.location__button');
 let modal = document.querySelector('.modal');
-let arrivalInput = document.querySelector('.arrive-input')
+let arrivalInput = document.querySelector('.arrive-input');
+let form = document.querySelector('.login-form');
+let departureInput = document.querySelector('.departure-input');
+let formLink = document.querySelector('.location__button');
 
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+formLink.removeAttribute('href');
 modal.classList.remove('modal--nojs');
 
 openButton.addEventListener("click", () => {
@@ -11,9 +24,24 @@ openButton.addEventListener("click", () => {
 
 window.addEventListener("keydown", (evt) => {
     if (evt.keyCode === 27) {
-      if (modal.classList.contains("modal-show")) {
+      if (modal.classList.contains('modal-show')) {
         evt.preventDefault();
-        modal.classList.remove("modal-show");
+        modal.classList.remove('modal-show');
+        modal.classList.remove('modal-error');
       }
     }
+});
+
+form.addEventListener("submit", (evt) => {
+  if (!arrivalInput.value || !departureInput.value) {
+    evt.preventDefault();
+    modal.classList.remove("modal-error");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", arrivalInput.value);
+      localStorage.setItem("departion", departureInput.value);
+    }
+  }
 });
